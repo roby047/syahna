b30315a3450d9ddbd2d2910e305eb55c0aa0faa0
roby047 <robyjaya047@gmail.com>
2020-06-02 10:25:01 +0700

    roby047 nambahin dar laravel tuk backend

---
+++ app/Models/Transaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['uuid', 'name', 'email', 'number', 'address', 'transaction_total', 'transaction_status'];
+
+    protected $hidden = [];
+
+    // mking relasi
+    public function details(){
+         return $this->hasMany(TransactionDetail::class, 'transactions_id');
+    }
+}
