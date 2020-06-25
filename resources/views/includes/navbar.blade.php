f2019c13c54f93d8e334436c45b95a4011f8e25f
roby047 <robyjaya047@gmail.com>
2020-06-25 15:11:49 +0700

    tes deployment

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="top-left">
       <div class="navbar-header">
           <a class="navbar-brand" href="./"><img src="{{ url('images/logo.png')}}" alt="Logo"></a>
-          <a class="navbar-brand hidden" href="./"><img src="{{ url('images/logo2.png')}}" alt="Logo"></a>
+          <a class="navbar-brand hidden" href="./"><img src="{{ url('images/LogoSyahna.png')}}" alt="Logo"></a>
           <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
       </div>
   </div>
